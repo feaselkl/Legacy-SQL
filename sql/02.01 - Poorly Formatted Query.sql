d9928884291cc216a0057fc7116d5eff15f1f0d3
Kevin Feasel <feasel@catallaxyservices.com>
2015-03-14 21:17:30 -0400

    Un-fixed whitespace
    Set the whitespace in the poorly-formatted example back to what it
    should have been.

--- sql/02.01 - Poorly Formatted Query.sql
+++ sql/02.01 - Poorly Formatted Query.sql
@@ -1,43 +1,43 @@
 SELECT
-    ISNULL(BasicID, 0) BasicID, JDate,krc.CustomerID,ISNULL(krc.SearchProviderID, 0) SearchProviderID,ISNULL(NULLIF(STCategoryID, '-1'), '0')
+	ISNULL(BasicID, 0) BasicID, JDate,krc.CustomerID,ISNULL(krc.SearchProviderID, 0) SearchProviderID,ISNULL(NULLIF(STCategoryID, '-1'), '0')
 STCategoryID,
-                krc.EventTypeID,
-    CAST(JDate AS DATETIME) CreateDate,
-    DATEPART(MONTH, CAST(JDate AS DATETIME)) CreateMonth,
+				krc.EventTypeID,
+	CAST(JDate AS DATETIME) CreateDate,
+	DATEPART(MONTH, CAST(JDate AS DATETIME)) CreateMonth,
 DATEPART(YEAR, CAST(JDate AS DATETIME)) CreateYear, Keyword, NULLIF(PageID, 0) PageID,
-    CASE
-        WHEN BasicID > 0 THEN 1
-        ELSE 0 END Paid,
-    CAST(MAX(ISNULL(s.SearchProviderID, 0)) AS BIT) AllowsCampaigns,
-    SUM
-    (ISNULL(Conversions
-    , 0)) AS Orders,  SUM(ISNULL
-    (Hits, 0)) PixelHits,
-        SUM(ISNULL(Clicks, 0)) Clicks,
-    SUM(
-        ISNULL(Impressions,
+	CASE
+		WHEN BasicID > 0 THEN 1
+		ELSE 0 END Paid,
+	CAST(MAX(ISNULL(s.SearchProviderID, 0)) AS BIT) AllowsCampaigns,
+	SUM
+	(ISNULL(Conversions
+	, 0)) AS Orders,  SUM(ISNULL
+	(Hits, 0)) PixelHits,
+		SUM(ISNULL(Clicks, 0)) Clicks,
+	SUM(
+		ISNULL(Impressions,
 0)) AS Impressions,
-    CAST(CASE SUM(ISNULL(Impressions, 0)) WHEN 0 THEN SUM(ISNULL(WeightedRank, 0)) ELSE SUM(ISNULL(WeightedRank, 0) * ISNULL(Impressions, 0)) / SUM(ISNULL(Impressions, 0)) END AS MONEY) WeightedRank,
-    SUM(CASE WHEN BasicID > 0 THEN 1 ELSE 0 END) AS AdCount,
-    BINARY_CHECKSUM(krc.CustomerID, ISNULL(BasicID, 0), ISNULL(NULLIF(STCategoryID, '-1'), '0')) AS CheckSumID,
-    RateAdjustmentCustomRuleID,
-    SUM(ISNULL(EstimatedCost, 0)) AS EstimatedCost,
-    SUM(ISNULL(EstimatedClicks, 0)) AS EstimatedClicks,
-    SUM(CASE WHEN ISNULL(et.ParentEventTypeID, 0) = 3 
-    THEN krc.Revenue ELSE 0 END) AS [OrderRevenue],
-    SUM(
-    CASE WHEN
-    ISNULL(et.ParentEventTypeID, 0) = 3 
-    THEN
-    krc.Conversions ELSE 0 
-    END) AS [OrderCount],
-    SUM(CASE WHEN ISNULL(et.ParentEventTypeID, 0) = 4 
-                THEN
-        krc.Revenue 
-                ELSE 0 
-            END)
-    [EventValue],
-    SUM(CASE WHEN ISNULL(et.ParentEventTypeID, 0) = 4 THEN krc.Conversions ELSE 0 END) AS [EventCount]
+	CAST(CASE SUM(ISNULL(Impressions, 0)) WHEN 0 THEN SUM(ISNULL(WeightedRank, 0)) ELSE SUM(ISNULL(WeightedRank, 0) * ISNULL(Impressions, 0)) / SUM(ISNULL(Impressions, 0)) END AS MONEY) WeightedRank,
+	SUM(CASE WHEN BasicID > 0 THEN 1 ELSE 0 END) AS AdCount,
+	BINARY_CHECKSUM(krc.CustomerID, ISNULL(BasicID, 0), ISNULL(NULLIF(STCategoryID, '-1'), '0')) AS CheckSumID,
+	RateAdjustmentCustomRuleID,
+	SUM(ISNULL(EstimatedCost, 0)) AS EstimatedCost,
+	SUM(ISNULL(EstimatedClicks, 0)) AS EstimatedClicks,
+	SUM(CASE WHEN ISNULL(et.ParentEventTypeID, 0) = 3 
+	THEN krc.Revenue ELSE 0 END) AS [OrderRevenue],
+	SUM(
+	CASE WHEN
+	ISNULL(et.ParentEventTypeID, 0) = 3 
+	THEN
+	krc.Conversions ELSE 0 
+	END) AS [OrderCount],
+	SUM(CASE WHEN ISNULL(et.ParentEventTypeID, 0) = 4 
+				THEN
+		krc.Revenue 
+				ELSE 0 
+			END)
+	[EventValue],
+	SUM(CASE WHEN ISNULL(et.ParentEventTypeID, 0) = 4 THEN krc.Conversions ELSE 0 END) AS [EventCount]
 FROM SomeTable krc INNER JOIN SomeOtherTable c2a ON krc.CustomerID = c2a.CustomerID AND c2a.EligibleForGC = 0
     LEFT OUTER JOIN
     (SELECT s.CustomerID, s.SearchProvider, s.SearchProviderID
